7703e1063ab2d3920cdbccc3bd1331dedc56101c
Rickard Gustafsson <rickard@allyourbase.se>
2017-05-29 20:49:39 +0200

    Remote control

--- ir.ino
+++ ir.ino
@@ -1,47 +1,79 @@
-/*
- * IRremoteESP8266: IRsendDemo - demonstrates sending IR codes with IRsend.
- *
- * An IR LED circuit *MUST* be connected to ESP8266 pin 4 (D2).
- *
- * TL;DR: The IR LED needs to be driven by a transistor for a good result.
- * 
- * Suggested circuit:
- *     https://github.com/markszabo/IRremoteESP8266/wiki#ir-sending
- *
- * Common mistakes & tips:
- *   * Don't just connect the IR LED directly to the pin, it won't
- *     have enough current to drive the IR LED effectively.
- *   * Make sure you have the IR LED polarity correct.
- *     See: https://learn.sparkfun.com/tutorials/polarity/diode-and-led-polarity
- *   * Typical digital camera/phones can be used to see if the IR LED is flashed.
- *     Replace the IR LED with a normal LED if you don't have a digital camera
- *     when debugging.
- *   * Avoid using the following pins unless you really know what you are doing:
- *     * Pin 0/D3: Can interfere with the boot/program mode & support circuits.
- *     * Pin 1/TX/TXD0: Any serial transmissions from the ESP8266 will interfere.
- *     * Pin 3/RX/RXD0: Any serial transmissions to the ESP8266 will interfere.
- *   * ESP-01 modules are tricky. We suggest you use a module with more GPIOs
- *     for your first time. e.g. ESP-12 etc.
- *
- * Version 1.0 April, 2017
- * Based on Ken Shirriff's IrsendDemo Version 0.1 July, 2009, Copyright 2009 Ken Shirriff, http://arcfn.com
- */
-
 #include <IRremoteESP8266.h>
+#include <ESP8266WiFi.h>
+#include "wifi.h"
+#include "codes.h"
 
-IRsend irsend(4); //an IR led is connected to GPIO pin 4 (D2)
+#define IR_SEND(BUTTON) do { irsend.sendLG(BUTTON, 32); blink(); } while(0)
+
+IRsend irsend(4);
+WiFiServer server(80);
 
 void setup()
 {
   irsend.begin();
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
+  pinMode(2, OUTPUT);
 }
 
-void loop() {
-  Serial.println("LG");
-  irsend.sendLG(0x20DF10EFUL, 32);
-  delay(100);
-  Serial.println("NEC");
-  irsend.sendNEC(0x20DF10EFUL, 32);
-  delay(2000);
+void blink(void)
+{
+    digitalWrite(2, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(2, LOW);
+    delay(100);
+    digitalWrite(2, HIGH);
+}
+
+void loop()
+{
+    if (WiFi.status() != WL_CONNECTED) {
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            Serial.print(".");
+            delay(300);
+            blink();
+        }
+        Serial.println("");
+        Serial.println(WiFi.localIP());
+        if (server.status() != 1) {
+            Serial.println("Strating server");
+            server.begin();
+            delay(1000);
+        }
+    }
+
+    Serial.println("Server status:");
+    Serial.println(server.status());
+
+    WiFiClient client = server.available();
+    if (!client) {
+        Serial.println("No client");
+        delay(1);
+        return;
+    }
+    
+    String req = client.readStringUntil('\r');
+    client.flush();
+
+#define X(name) \
+    if (req.indexOf(#name) != -1) \
+        IR_SEND(name);
+
+LIST_CONTROLS
+#undef X
+    
+    client.print("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n \ 
+            <!DOCTYPE HTML>\r\n<html>\r\n");
+
+#define X(name) client.print("<a href=\""); \
+    client.print("/"#name); \
+    client.print("\">"); \
+    client.print(#name); \
+    client.print("</a><br />\r\n");
+
+LIST_CONTROLS
+#undef X
+
+    client.print("</html>\n");
+    delay(10);
 }
